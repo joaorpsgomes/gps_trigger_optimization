a3fc362a9b8e2fd7276e87fdfab204f5e0522c1f
joao gomes <joaorpsgomes@gmail.com>
2022-01-26 16:12:14 +0000

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joaorpsgomes/ros_tese_noetic/src/gps_optimize_tool
-# Build directory: /home/joaorpsgomes/ros_tese_noetic/src/gps_optimize_tool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/joaorpsgomes/ros_tese_noetic/src/gps_optimize_tool/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
